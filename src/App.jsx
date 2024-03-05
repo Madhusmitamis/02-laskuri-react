11f973f266a925e8f89c77bebcf68fbc26475202
Madhu <mrs.madhusmita.mishra@gmail.com>
2024-03-05 14:40:11 +0200

    display component is added in passing state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
-  
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
       <button onClick={increaseByOne}>
         plus
       </button>
