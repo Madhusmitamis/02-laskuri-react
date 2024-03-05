1fd4ebf0aeb51041ac39506f226865271462baa7
Madhu <mrs.madhusmita.mishra@gmail.com>
2024-03-05 14:46:25 +0200

    event handler is passed to  Button component through the onClick prop.

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,35 @@ const Display = (props) => {
     <div>{props.counter}</div>
   )
 }
-
-
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+  }
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
       <Display counter={counter}/>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
