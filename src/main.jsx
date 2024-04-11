baebd56a5a1fdd98068bf3ccd67634974d4546df
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-04-11 11:48:24 +0300

    App fixed.

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,20 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-let counter = 1
+// let counter = 1
 
-const refresh = () => {
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App counter={counter}/>)
+  <App />)
+  // <App counter={counter}/>)
   
-}
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+// setInterval(() => {
+//   refresh()
+//   counter += 1
+// }, 1000)
